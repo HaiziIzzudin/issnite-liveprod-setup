e6d534d4ee14d783cbdf39ed1af30132a44591d3
haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2024-08-16 23:45:51 +0800

    Add files via upload

---
+++ open_all_apps.bat
@@ -0,0 +1,6 @@
+@echo off
+start /b python script.py 
+timeout 6
+start "" https://insert-direct-slash-edit-canva-link-here
+start "" "C:\Users\haizi\Desktop\Other Shortcuts\OBS Studio.lnk"
+PAUSE
---
+++ run_python_script_only.bat
@@ -0,0 +1,3 @@
+@echo off
+start /b python script.py 
+PAUSE
